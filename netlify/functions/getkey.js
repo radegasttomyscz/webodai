feaa3d829a71be81c7d3bd63164e0628cabfc855
radegasttomyscz <tomyscz1@gmail.com>
2026-05-06 13:34:24 +0200

    Delete netlify/functions directory

--- netlify/functions/getkey.js
+++
@@ -1,7 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify({ key: process.env.ANTHROPIC_API_KEY }),
-  };
-}
